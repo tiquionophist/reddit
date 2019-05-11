0a80e98be4878f705ce02b90da9b46c63bec13bd
tiquionophist <tiquionophist@gmail.com>
2019-05-11 14:52:35 -0700

    Improving MediaProvider matching tests

--- src/test/kotlin/com/tiquionophist/reddit/mediaproviders/DirectLinkTest.kt
+++ src/test/kotlin/com/tiquionophist/reddit/mediaproviders/DirectLinkTest.kt
@@ -1,27 +1,41 @@
 package com.tiquionophist.reddit.mediaproviders
 
+import com.tiquionophist.reddit.UrlPermuter
 import com.tiquionophist.reddit.assertDoesNotMatch
 import com.tiquionophist.reddit.assertMatches
 import org.junit.jupiter.api.Test
 
 internal class DirectLinkTest {
 
+    companion object {
+
+        private val urls = UrlPermuter(host = "any.website", allowTrailingSlash = false)
+
+        private val validPaths = listOf(
+            "abcd.png",
+            "abcd.jpeg",
+            "abcd.png",
+            "abcd.gif",
+            "abcd.mp4",
+            "abcd.webm",
+            "abcd.png?1#2"
+        )
+        private val invalidPaths = listOf(
+            "",
+            " ",
+            "abcd",
+            "abcd.txt",
+            "abcd.gifv",
+            "abcd.png.nothing",
+            "a1b2c3",
+            "1+1=2",
+            "a b"
+        )
+    }
+
     @Test
     fun testMatches() {
-        // TODO flesh these out
-        DirectLink.assertMatches("https://i.imgur.com/abcd.png")
-        DirectLink.assertMatches("https://i.imgur.com/abcd.jpg")
-        DirectLink.assertMatches("https://i.imgur.com/abcd.gif")
-        DirectLink.assertMatches("https://i.imgur.com/abcd.mp4")
-        DirectLink.assertMatches("https://i.imgur.com/abcd.webm")
-
-        DirectLink.assertMatches("http://any.website/abcd.png")
-        DirectLink.assertMatches("https://i.imgur.com/abcd.png?1#2")
-
-        DirectLink.assertDoesNotMatch("https://i.imgur.com")
-        DirectLink.assertDoesNotMatch("https://i.imgur.com/abcd")
-        DirectLink.assertDoesNotMatch("https://i.imgur.com/abcd.txt")
-        DirectLink.assertDoesNotMatch("https://i.imgur.com/abcd.gifv")
-        DirectLink.assertDoesNotMatch("https://i.imgur.com/abcd.png.nothing")
+        urls.permute(validPaths).forEach { DirectLink.assertMatches(it) }
+        urls.permute(invalidPaths).forEach { DirectLink.assertDoesNotMatch(it) }
     }
 }
--- src/test/kotlin/com/tiquionophist/reddit/mediaproviders/RedditTest.kt
+++ src/test/kotlin/com/tiquionophist/reddit/mediaproviders/RedditTest.kt
@@ -1,21 +1,26 @@
 package com.tiquionophist.reddit.mediaproviders
 
+import com.tiquionophist.reddit.UrlPermuter
 import com.tiquionophist.reddit.assertDoesNotMatch
 import com.tiquionophist.reddit.assertMatches
 import org.junit.jupiter.api.Test
 
 internal class RedditTest {
 
+    companion object {
+
+        private val urls = UrlPermuter(host = "i.reddituploads.com", allowWww = false)
+    }
+
     @Test
     fun testMatches() {
-        Reddit.assertMatches("http://i.reddituploads.com")
-        Reddit.assertMatches("http://i.reddituploads.com/any1hing?1#2")
+        urls.permute(listOf("", "any1hing?1#2")).forEach { Reddit.assertMatches(it) }
 
         Reddit.assertDoesNotMatch("https://reddit.com/")
-        Reddit.assertDoesNotMatch("https://external-preview.redd.it/abc")
         Reddit.assertDoesNotMatch("https://reddit.com/abc")
         Reddit.assertDoesNotMatch("https://reddit.com/r/abc")
         Reddit.assertDoesNotMatch("https://reddit.com/u/abc")
+        Reddit.assertDoesNotMatch("https://external-preview.redd.it/abc")
         Reddit.assertDoesNotMatch("http://v.redd.it/abc")
     }
 }
